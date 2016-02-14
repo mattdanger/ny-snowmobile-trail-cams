d9023a07edf0d02ad925bc02e7cf7e3645114ea7
Matt West <matt@mattdanger.net>
2016-02-13 20:44:27 -0500

    Updates.

--- angular/app.js
+++ angular/app.js
@@ -10,11 +10,12 @@ app.config(function(uiGmapGoogleMapApiProvider) {
 
 app.controller('nystc_list', function($scope, $http, $interval, uiGmapGoogleMapApi) {
 
-
-  $scope.location = null;  
-
   load_locations();
 
+
+  /**
+   * Load camera locations
+   */
   function load_locations(){
 
     $http.get('/api/locations').success(function(data) {
@@ -27,19 +28,26 @@ app.controller('nystc_list', function($scope, $http, $interval, uiGmapGoogleMapA
 
         // Init map    
         $scope.map = { center: { latitude: 43.592026, longitude: -75.670652 }, zoom: 8 };
+
         $scope.locMarkers = [];
+
         $scope.onClick = function(marker, eventName, model) {
-console.log(model);
+
           model.show = !model.show;
+
         };
+
         $scope.$watch(function() {
+
           return $scope.map.bounds;
+
         }, function(nv, ov) {
 
           var locMarkers = [];
           for (var i = 0; i < $scope.locations.length; i++) {
 
             var l = $scope.locations[i];      
+
             var marker = {
               id: i,
               latitude: l.lat,
